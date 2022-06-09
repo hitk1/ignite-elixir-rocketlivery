4cf46ded8a14baf313a78ff132f60bc2637084d7
hitk1 <luispaulo.degini@gmail.com>
2022-06-09 09:07:40 -0300

    file: configuracoes gerais do app

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :rocketlivery, Rocketlivery.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "rpi1234",
   database: "rocketlivery_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
