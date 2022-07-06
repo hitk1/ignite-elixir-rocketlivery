821562b9bf8c974b036f615cb753e3fd66a6ec21
hitk1 <luispaulo.degini@gmail.com>
2022-07-05 23:20:08 -0300

    feat: setup do client http

---
+++ lib/via_cep/client.ex
@@ -0,0 +1,29 @@
+defmodule Rocketlivery.ViaCep.Client do
+  use Tesla
+
+  alias Tesla.Env, as: TeslaEnv
+  alias Rocketlivery.Error
+
+  # Configurando a base URL que este modulo vai usar
+  plug Tesla.Middleware.BaseUrl, "https://viacep.com.br/ws"
+
+  # Middleware de manipualação dos payload, tanto envio quando resposta serão encodados pra JSON
+  plug Tesla.Middleware.JSON
+
+  def get_cep_info(cep) do
+    get("#{cep}/json")
+    |> handle_response()
+  end
+
+  defp handle_response({:ok, %TeslaEnv{status: 400, body: _any}}) do
+    {:error, Error.build(:bad_request, "Invalid CEP")}
+  end
+
+  defp handle_response({:ok, %TeslaEnv{status: 200, body: %{"erro" => "true"}}}) do
+    {:error, Error.build(:bad_request, "CEP not found!")}
+  end
+
+  defp handle_response({:ok, %TeslaEnv{status: 200, body: body}}), do: {:ok, body}
+
+  defp handle_response({:error, reason}), do: {:error, Error.build(:bad_request, reason)}
+end
