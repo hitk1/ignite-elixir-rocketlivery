5b19d2a5a155a5db8660261b39e0f2e44ac7a4be
hitk1 <luispaulo.degini@gmail.com>
2022-07-16 18:32:30 -0300

    test: configs testes

--- lib/rocketlivery/users/create.ex
+++ lib/rocketlivery/users/create.ex
@@ -6,12 +6,19 @@ defmodule Rocketlivery.Users.Create do
     cep = Map.get(params, "cep")
 
     with {:ok, %User{} = user} <- User.build(params),
-         {:ok, _cep_data} <- ViaCepClient.get_cep_info(cep) do
+         {:ok, _cep_data} <- client().get_cep_info(cep) do
       Repo.insert(user)
     else
       {:error, %Error{}} = error -> error
       {:error, reason} -> {:error, Error.build(:bad_request, reason)}
     end
+  end
+
+  defp client do
+    # Le as configurações feitas no arquivo geral de configuraçoes para selecionar dados dinamicos
+    :rocketlivery
+    |> Application.fetch_env!(__MODULE__)
+    |> Keyword.get(:via_cep_adapter)
   end
 
   # defp create_user(params) do
--- test/rocketlivery/users/create_test.exs
+++ test/rocketlivery/users/create_test.exs
@@ -1,13 +1,29 @@
 defmodule Rocketlivery.Users.CreateTest do
   use Rocketlivery.DataCase, async: true
+
   alias Rocketlivery.{Error, User}
   alias Rocketlivery.Users.Create
+  alias Rocketlivery.ViaCep.ClientMock
 
+  import Mox
   import Rocketlivery.Factory
 
   describe "call/1" do
     test "when all params are valid, returns the user" do
       params = build(:user_params)
+
+      expect(ClientMock, :get_cep_info, fn _cep ->
+        {:ok,
+         %{
+           "age" => 24,
+           "address" => "Rua teste do tste, 34",
+           "cep" => "12345688",
+           "cpf" => "12345678900",
+           "email" => "test@mail.com",
+           "password" => "123123",
+           "name" => "Testing app"
+         }}
+      end)
 
       response = Create.call(params)
 
