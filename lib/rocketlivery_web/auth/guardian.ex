1d46ea63ecc6fb55d9a2fcc2c49d251b861bbd58
hitk1 <luispaulo.degini@gmail.com>
2022-07-17 17:37:55 -0300

    feat: funcao para autenticacao de usuario

--- lib/rocketlivery_web/auth/guardian.ex
+++ lib/rocketlivery_web/auth/guardian.ex
@@ -1,7 +1,7 @@
 defmodule RocketliveryWeb.Auth.Guardian do
   use Guardian, otp_app: :rocketlivery
 
-  alias Rocketlivery.User
+  alias Rocketlivery.{User, Error}
   alias Rocketlivery.Users.Get, as: GetUser
 
   def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
@@ -9,4 +9,17 @@ defmodule RocketliveryWeb.Auth.Guardian do
   def resource_from_claims(%{"sub" => id}) do
     GetUser.by_id(id)
   end
+
+  def authenticate(%{"id" => id, "password" => password}) do
+    with {:ok, %User{password_hash: hash} = user} <- GetUser.by_id(id),
+         true <- Pbkdf2.verify_pass(password, hash),
+         {:ok, token, _claims} <- encode_and_sign(user) do
+      {:ok, token}
+    else
+      false -> {:error, Error.build(:unauthorized, "Please verify your credentials")}
+      error -> error
+    end
+  end
+
+  def authenticate(_), do: {:error, Error.build(:bad_request, "Invalid or missing params")}
 end
